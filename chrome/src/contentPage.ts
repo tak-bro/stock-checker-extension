28e25b15ef7c1bf83873d058bf6d857137aa3862
Tak <env.tak@gmail.com>
2019-11-12 12:28:42 +0900

    feat: filter message on contentPage

--- chrome/src/contentPage.ts
+++ chrome/src/contentPage.ts
@@ -1,5 +1,5 @@
 import { of } from 'rxjs';
-import { filter, map, switchMap, tap } from 'rxjs/operators';
+import { delay, filter, map, switchMap, tap } from 'rxjs/operators';
 
 const ALLOWED_MESSAGE = ['CONTENT_CHECK_PRODUCT_FORM', 'CONTENT_CHECK_CART_FORM'];
 
@@ -16,50 +16,52 @@ chrome.runtime.onMessage.addListener((request, sender, respond) => {
     }
 
     // STEP 1
-    if (message === 'CONTENT_CHECK_PRODUCT_FORM') {
-        let cartFormElement = document.getElementById('addToCartFormHolder');
-        const checkCartFormElement$ = of(cartFormElement).pipe(
-            tap(cartForm => {
-                if (!cartForm) {
-                    cartFormElement = document.getElementById('addToCartFormHolder');
-                }
-            }),
-            filter(cartForm => cartForm.classList ? true : false),
-            map(cartForm => cartForm.classList.contains('hide')) // check form element
-        );
+    const isProductMessage$ = of(message).pipe(filter(message => message === 'CONTENT_CHECK_PRODUCT_FORM'));
+    const isCartMessage$ = of(message).pipe(filter(message => message === 'CONTENT_CHECK_CART_FORM'));
 
-        const addToCart$ = checkCartFormElement$.pipe(
-            filter(outOfStock => !outOfStock), // in stock
-            switchMap(() => of(document.getElementById('addToCartSubmit'))),
-            tap(addToCartButtonElement => addToCartButtonElement.click()),
-            // switchMap(addToCartButtonElement => fromEvent(addToCartButtonElement, 'click')), // TODO: add clicked event listener
-            map(() => 'SUCCESS'),
-        );
+    let cartFormElement = document.getElementById('addToCartFormHolder');
+    const checkCartFormElement$ = isProductMessage$.pipe(
+        switchMap(() => of(cartFormElement)),
+        tap(cartForm => {
+            if (!cartForm) {
+                cartFormElement = document.getElementById('addToCartFormHolder');
+            }
+        }),
+        filter(cartForm => cartForm.classList ? true : false),
+        map(cartForm => cartForm.classList.contains('hide')) // check form element
+    );
 
-        const refreshPage$ = checkCartFormElement$.pipe(
-            filter(outOfStock => outOfStock),
-            map(() => 'REFRESH')
-        );
+    const addToCart$ = checkCartFormElement$.pipe(
+        filter(outOfStock => !outOfStock), // in stock
+        switchMap(() => of(document.getElementById('addToCartSubmit'))),
+        tap(addToCartButtonElement => addToCartButtonElement.click()),
+        // switchMap(addToCartButtonElement => fromEvent(addToCartButtonElement, 'click')), // TODO: add clicked event listener
+        delay(500),
+        map(() => 'SUCCESS'),
+    );
 
-        addToCart$.subscribe(res => respond(res));
-        refreshPage$.subscribe(res => respond(res));
-    }
+    const refreshPage$ = checkCartFormElement$.pipe(
+        filter(outOfStock => outOfStock),
+        map(() => 'REFRESH')
+    );
+
+    addToCart$.subscribe(res => respond(res));
+    refreshPage$.subscribe(res => respond(res));
 
     // STEP 2
-    if (message === 'CONTENT_CHECK_CART_FORM') {
-        let proceedToCheckoutButton = document.getElementById('proceedToCheckoutButton');
-        const canProceedToCheckout$ = of(proceedToCheckoutButton).pipe(
-            tap(checkoutButton => {
-                if (!checkoutButton) {
-                    proceedToCheckoutButton = document.getElementById('proceedToCheckoutButton');
-                }
-            }),
-            filter(button => button ? true : false),
-            map((checkoutButton: any) => checkoutButton.disabled ? 'REFRESH' : 'SUCCESS')
-        );
+    let proceedToCheckoutButton = document.getElementById('proceedToCheckoutButton');
+    const canProceedToCheckout$ = isCartMessage$.pipe(
+        switchMap(() => of(proceedToCheckoutButton)),
+        tap(checkoutButton => {
+            if (!checkoutButton) {
+                proceedToCheckoutButton = document.getElementById('proceedToCheckoutButton');
+            }
+        }),
+        filter(button => button ? true : false),
+        map((checkoutButton: any) => checkoutButton.disabled ? 'REFRESH' : 'SUCCESS')
+    );
 
-        canProceedToCheckout$.subscribe(res => respond(res));
-    }
+    canProceedToCheckout$.subscribe(res => respond(res));
 
     return true;
 });
